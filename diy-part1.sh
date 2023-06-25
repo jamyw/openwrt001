5e5b43511c37876b8299b22cb7a7387921b1000d
jamyw <134950769+jamyw@users.noreply.github.com>
2023-06-25 17:58:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,6 +28,7 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 ./scripts/feeds uninstall luci-app-mosdns mosdns v2ray-geodata
 ./scripts/feeds install -f -p mosdns mosdns luci-app-mosdns
+
 find ./ -name v2ray-geodata | xargs rm -rf
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 svn export https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-vlmcsd package/luci-app-vlmcsd
